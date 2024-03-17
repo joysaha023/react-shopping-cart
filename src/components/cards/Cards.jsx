4a39a70901a941da3261d7e560e5c14d56d82586
Joy <github.joysaha@gmail.com>
2024-03-17 13:28:23 +0600

    last and final commit

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,7 +1,7 @@
 import Card from '../card/Card'
 
 const cards = ({cartsData, handleClick}) => {
-    // console.log(cartsData)
+    
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
             {
